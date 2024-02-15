87557cf5890a27f850d7903ec5a34051980d8a55
khalid marzoug <149273608+khalidmarzou@users.noreply.github.com>
2024-02-15 12:45:22 +0100

    create data base , create table , insert into a table , change values and drop some values

--- Script SQL/GestionVol.sql
+++ Script SQL/GestionVol.sql
@@ -21,4 +21,27 @@ select * from avion where localite = 'RABAT' or localite = 'CASA';
 select * from avion where localite in ('RABAT','CASA');
 -- Modifier une valeur :
 update avion set capacite = 220 where NA = 101;
+-- supprimer les avions dont la capacite est inferieure a 200 :
+delete from avion where capacite < 200;
+-- Afficher la capacite max, min et moy des avions :
+select max(capacite),min(capacite),avg(capacite) from avion;
 
+------------------------------------------------------------
+insert into vol (NV, NA, VD, VA,HD,HA) values ('IT100', 100, 'Casablanca', 'Marrakech', 1200, 1400),
+						('IT101', 101, 'Rabat', 'Casablanca', 1330, 1500),
+						('IT102', 101, 'Casablanca', 'Rabat', 1400, 1530),
+						('IT103', 100, 'Marrakech', 'Casablanca', 1500, 1700),
+						('IT104', 101, 'Casablanca', 'Marrakech', 1600, 1800);
+select * from vol;
+insert into pilote values (1, 'Karim IDRISSI', '12 Rue Atlas Casablanca'),
+							(2, 'Fatima Benali', '789 Sahara Avenue Marrakech'),
+							(3, 'Ahmed Oujdaoui', '42 Rif Road Rabat'),
+							(4, 'Amal','Tanger');
+select * from pilote;
+update vol set NP = case NV 		when NV = 'IT100' then NP = 1
+									when NV = 'IT101' then NP = 2
+									when NV = 'IT102' then NP = 3
+									when NV = 'IT103' then NP = 1
+									when NV = 'IT104' then NP = 2
+									end
+									where NV = NP;
