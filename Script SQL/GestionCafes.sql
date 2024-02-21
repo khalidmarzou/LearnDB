b1294847a8e41270ecd25c74ae44906148bb27d3
khalid marzoug <KHALIDMARZOUG9@gmail.com>
2024-02-21 07:15:43 +0100

    linking prostgresSQL with vs code , and test server in vs code , from today change the platforme of working

--- Script SQL/GestionCafes.sql
+++ Script SQL/GestionCafes.sql
@@ -1,17 +1,66 @@
-create table employes (id int primary key, first_name varchar(20),
-					   last_name varchar(20), email varchar(20), hire_date date,
-					   gender varchar(1), salary int /* coffeShop_id int foreign key from table shops */);
-					   
-create table shops (coffeeshop_id int primary key, coffeeshop_name varchar(20)
-					/* city_id int foreign key from table city*/);
-					
+CREATE TABLE
+  employes (
+    id INT PRIMARY key,
+    first_name VARCHAR(20),
+    last_name VARCHAR(20),
+    email VARCHAR(20),
+    hire_date DATE,
+    gender VARCHAR(1),
+    salary INT /* coffeShop_id int foreign key from table shops */
+  );
+
+
+
+CREATE TABLE
+  shops (
+    coffeeshop_id INT PRIMARY key,
+    coffeeshop_name VARCHAR(20)
+    /* city_id int foreign key from table city*/
+  );
+
+
+
 /* ON DELETE CASCADE is used to specify that when a row is deleted from the parent table,
-	all rows in the child table that reference the deleted row should also be deleted.
-	This is useful for maintaining the integrity of the database. */
+all rows in the child table that reference the deleted row should also be deleted.
+This is useful for maintaining the integrity of the database. */
 -- to  create a new column :
-alter table employes 
-	add column coffeeshop int;
+ALTER TABLE employes
+ADD COLUMN coffeeshop INT;
+
+
+
 -- to add a foreign key :
-alter table employes
-	add foreign key (coffeeshop) references shops(coffeeshop_id);
-	
+ALTER TABLE employes add FOREIGN key (coffeeshop) REFERENCES shops (coffeeshop_id);
+
+
+
+ALTER TABLE employes
+RENAME COLUMN coffeeshop TO coffeeshop_id;
+
+
+
+SELECT
+  *
+FROM
+  employes;
+
+
+
+SELECT
+  *
+FROM
+  shops;
+
+
+
+/* The ON DELETE CASCADE is a referential action that can be specified when defining a foreign key constraint in a database.
+When applied to a foreign key relationship between two tables, it indicates that if a record in the parent table (referenced table) is deleted,
+then all corresponding records in the child table (referencing table) will be automatically deleted as well.*/
+/* **RESTRICT**: This option prevents the deletion or update of a referenced row if there are any dependent rows in the child table. It acts as
+a safeguard against accidental deletion or modification of important data.
+ **SET NULL**: With this option, if a referenced row is deleted or updated, the foreign key column in the child table will be set to NULL.
+This is useful when you want to allow rows in the child table to exist without a corresponding parent row.
+ **SET DEFAULT**: Similar to `SET NULL`, this option sets the foreign key column in the child table to a default value specified during
+table creation if the referenced row is deleted or updated.
+ **NO ACTION**: This option is similar to `RESTRICT` in that it prevents the deletion or update of a referenced row
+if dependent rows exist in the child table. It's often the default behavior if no action is explicitly specified.*/
--- Script SQL/GestionVol.sql
+++ Script SQL/GestionVol.sql
@@ -1,87 +1,394 @@
 -- Creation des tables :
-create table AVION (NA int primary key, Nom varchar(12), capacite int, Localite varchar(10));
-create table PILOTE (NP int primary key, Nom varchar(25), Adresse varchar(40));
-create table VOL (NV int primary key, NP int, NA int, VD varchar (10), VA varchar(10),HD int,HA int,
-				  foreign key (NA) references AVION(NA), foreign key (NP) references PILOTE(NP));
+CREATE TABLE
+  AVION (
+    NA INT PRIMARY key,
+    Nom VARCHAR(12),
+    capacite INT,
+    Localite VARCHAR(10)
+  );
+
+
+
+CREATE TABLE
+  PILOTE (
+    NP INT PRIMARY key,
+    Nom VARCHAR(25),
+    Adresse VARCHAR(40)
+  );
+
+
+
+CREATE TABLE
+  VOL (
+    NV INT PRIMARY key,
+    NP INT,
+    NA INT,
+    VD VARCHAR(10),
+    VA VARCHAR(10),
+    HD INT,
+    HA INT,
+    FOREIGN key (NA) REFERENCES AVION (NA),
+    FOREIGN key (NP) REFERENCES PILOTE (NP)
+  );
+
+
+
 -- change type of an attribut :
-alter table VOL
-	alter column NV type varchar(6);
+ALTER TABLE VOL
+ALTER COLUMN NV type VARCHAR(6);
+
+
+
 -- insert into the table :
-insert into AVION values (100, 'AIRBUS',300,'RABAT'), (101,'B737',220,'CASA'), (102, 'B737',220, 'RABAT');
+INSERT INTO
+  AVION
+VALUES
+  (100, 'AIRBUS', 300, 'RABAT'),
+  (101, 'B737', 220, 'CASA'),
+  (102, 'B737', 220, 'RABAT');
+
+
+
 -- Afficher tous les avions :
-select * from avion;
+SELECT
+  *
+FROM
+  avion;
+
+
+
 -- Afficher tous les avions par ordre sur le nom :
-select * from avion order by Nom;
+SELECT
+  *
+FROM
+  avion
+ORDER BY
+  Nom;
+
+
+
 -- Afficher les noms te les capacites des avion :
-select Nom,capacite from avion;
+SELECT
+  Nom,
+  capacite
+FROM
+  avion;
+
+
+
 -- Afficher les localites des avions sans redondance:
-select distinct localite from avion;
+SELECT DISTINCT
+  localite
+FROM
+  avion;
+
+
+
 -- Afficher les avions dans la localite et Rabat ou Casa :
-select * from avion where localite = 'RABAT' or localite = 'CASA';
-select * from avion where localite in ('RABAT','CASA');
+SELECT
+  *
+FROM
+  avion
+WHERE
+  localite='RABAT' OR
+  localite='CASA';
+
+
+
+SELECT
+  *
+FROM
+  avion
+WHERE
+  localite IN ('RABAT', 'CASA');
+
+
+
 -- Modifier une valeur :
-update avion set capacite = 220 where NA = 101;
+UPDATE avion
+SET
+  capacite=220
+WHERE
+  NA=101;
+
+
+
 -- supprimer les avions dont la capacite est inferieure a 200 :
-delete from avion where capacite < 200;
+DELETE FROM avion
+WHERE
+  capacite<200;
+
+
+
 -- Afficher la capacite max, min et moy des avions :
-select max(capacite),min(capacite),avg(capacite) from avion;
+SELECT
+  MAX(capacite),
+  MIN(capacite),
+  AVG(capacite)
+FROM
+  avion;
+
+
 
 ------------------------------------------------------------
-insert into vol (NV, NA, VD, VA,HD,HA) values ('IT100', 100, 'Casablanca', 'Marrakech', 1200, 1400),
-						('IT101', 101, 'Rabat', 'Casablanca', 1330, 1500),
-						('IT102', 101, 'Casablanca', 'Rabat', 1400, 1530),
-						('IT103', 100, 'Marrakech', 'Casablanca', 1500, 1700),
-						('IT104', 101, 'Casablanca', 'Marrakech', 1600, 1800);
-select * from vol;
-insert into pilote values (1, 'Karim IDRISSI', '12 Rue Atlas Casablanca'),
-							(2, 'Fatima Benali', '789 Sahara Avenue Marrakech'),
-							(3, 'Ahmed Oujdaoui', '42 Rif Road Rabat'),
-							(4, 'Amal','Tanger');
-select * from pilote;
-update vol set NP = 1 where nv = 'IT100';
-update vol set NP = 2 where nv = 'IT101';
-update vol set NP = 3 where nv = 'IT102';
-update vol set NP = 1 where nv = 'IT103';
-update vol set NP = 2 where nv = 'IT104';
+INSERT INTO
+  vol (NV, NA, VD, VA, HD, HA)
+VALUES
+  (
+    'IT100',
+    100,
+    'Casablanca',
+    'Marrakech',
+    1200,
+    1400
+  ),
+  (
+    'IT101',
+    101,
+    'Rabat',
+    'Casablanca',
+    1330,
+    1500
+  ),
+  (
+    'IT102',
+    101,
+    'Casablanca',
+    'Rabat',
+    1400,
+    1530
+  ),
+  (
+    'IT103',
+    100,
+    'Marrakech',
+    'Casablanca',
+    1500,
+    1700
+  ),
+  (
+    'IT104',
+    101,
+    'Casablanca',
+    'Marrakech',
+    1600,
+    1800
+  );
+
+
+
+SELECT
+  *
+FROM
+  vol;
+
+
+
+INSERT INTO
+  pilote
+VALUES
+  (
+    1,
+    'Karim IDRISSI',
+    '12 Rue Atlas Casablanca'
+  ),
+  (
+    2,
+    'Fatima Benali',
+    '789 Sahara Avenue Marrakech'
+  ),
+  (
+    3,
+    'Ahmed Oujdaoui',
+    '42 Rif Road Rabat'
+  ),
+  (4, 'Amal', 'Tanger');
+
+
+
+SELECT
+  *
+FROM
+  pilote;
+
+
+
+UPDATE vol
+SET
+  NP=1
+WHERE
+  nv='IT100';
+
+
+
+UPDATE vol
+SET
+  NP=2
+WHERE
+  nv='IT101';
+
+
+
+UPDATE vol
+SET
+  NP=3
+WHERE
+  nv='IT102';
+
+
+
+UPDATE vol
+SET
+  NP=1
+WHERE
+  nv='IT103';
+
+
+
+UPDATE vol
+SET
+  NP=2
+WHERE
+  nv='IT104';
+
+
+
 -- selectionner les pilotes quit conduit les vol 100 et 104 :
-select pilote.nom, pilote.adresse from pilote
-	join vol on (vol.np = pilote.np)
-	where vol.NV in ('IT100','IT104');
+SELECT
+  pilote.nom,
+  pilote.adresse
+FROM
+  pilote
+  JOIN vol ON (vol.np=pilote.np)
+WHERE
+  vol.NV IN ('IT100', 'IT104');
+
+
+
 -- where :  est pour ajouter une condition ; null ; is not null ; between ; like : where first_name like 'A%' 
 -- on utilise % pour signife plusieurs caracteres ; _e_n__ on utilise _ si en connais la position exact des caracteres
 -- where price between 10 and 15 ; we can use between date , 10 and 15 est inclus dans range;
 -- AS (Alias) : select product_name AS "Mes Produits" from products, creer une neauvaux colone temporaire;
 -- EX d'APP :
 -- Afficher tous les enregistrements de la table VOL :
-select * from vol;
+SELECT
+  *
+FROM
+  vol;
+
+
+
 -- Afficher le nom et l'adresse de tous les pilotes :
-select nom, adresse from pilote;
+SELECT
+  nom,
+  adresse
+FROM
+  pilote;
+
+
+
 -- Afficher les vols dont l'heures de départ est apres 14h :
-select * from vol where hd >= 1400;
+SELECT
+  *
+FROM
+  vol
+WHERE
+  hd>=1400;
+
+
+
 -- Afficher les pilotes dont le nom commence par "A" :
-select * from pilote where nom like 'A%';
+SELECT
+  *
+FROM
+  pilote
+WHERE
+  nom LIKE 'A%';
+
+
+
 -- Afficher les avions bases a Rabat, avec la localité renommée en "Emplacement" :
-select na,nom,capacite,localite as Emplacement from avion where localite = 'RABAT';
+SELECT
+  na,
+  nom,
+  capacite,
+  localite AS Emplacement
+FROM
+  avion
+WHERE
+  localite='RABAT';
+
+
+
 -- Afficher tous les vols tries par la ville de depart :
-select * from vol order by vd; -- desc descendent ; asc ascendent ;
+SELECT
+  *
+FROM
+  vol
+ORDER BY
+  vd;
+
+
+
+-- desc descendent ; asc ascendent ;
 -- group by est utilise pour grouper plusieurs resultats et utiliser une fonction sur un groupe de resultat.
 -- group by on utilise having au place du where ;
 -- limit utilise pour specifier le nombre max de resultats que l on sauhaite obtenir.
 -- selct * from table limit 10 offset 5 , limit c est le nembre de lignes afficher et offset est witch line de depart.
 -- Jointures :
 /* inner join ; from table1 inner join table2 on (condition of the same column in the two tables);
-on resume dans : table1 jointure avec table2 on condition ,
+on resume dans : table1 jointure avec table2 on condition         ,
 il est possible de fait select from table1,table2 where condition*/
 -- EX APP :
 -- Afficher le nom et l'adresse des pilotes assuant les vols IT104 et IT100 :
-select nom, adresse from pilote inner join vol on pilote.np = vol.np where vol.nv in ('IT100','IT104');
+SELECT
+  nom,
+  adresse
+FROM
+  pilote
+  INNER JOIN vol ON pilote.np=vol.np
+WHERE
+  vol.nv IN ('IT100', 'IT104');
+
+
+
 -- Afficher le nombre total de vols effectués par chaque pilote avec le nom du pilote :
-select pilote.nom, count(vol.np) as nombreVol -- on work with group by always when we have a function in the requette
-	from pilote inner join vol on pilote.np = vol.np group by pilote.nom; --chaque est automatiquement un group by;
+SELECT
+  pilote.nom,
+  COUNT(vol.np) AS nombreVol -- on work with group by always when we have a function in the requette
+FROM
+  pilote
+  INNER JOIN vol ON pilote.np=vol.np
+GROUP BY
+  pilote.nom;
+
+
+
+--chaque est automatiquement un group by;
 -- Afficher les noms des pilotes qui conduisent un AIRBUS :
-	-- Ma Methode :
-select distinct pilote.nom from (select * from vol inner join avion on vol.na = avion.na)  as avionVolTable
-	inner join pilote on pilote.np = avionVolTable.np where avionVolTable.nom like 'AIRBUS';
-	-- Methode Simple :
-select distinct pilote.nom from pilote join vol on pilote.np = vol.np
-	inner join avion on vol.na = avion.na where avion.nom like 'AIRBUS';
+-- Ma Methode :
+SELECT DISTINCT
+  pilote.nom
+FROM
+  (
+    SELECT
+      *
+    FROM
+      vol
+      INNER JOIN avion ON vol.na=avion.na
+  ) AS avionVolTable
+  INNER JOIN pilote ON pilote.np=avionVolTable.np
+WHERE
+  avionVolTable.nom LIKE 'AIRBUS';
+
+
+
+-- Methode Simple :
+SELECT DISTINCT
+  pilote.nom
+FROM
+  pilote
+  JOIN vol ON pilote.np=vol.np
+  INNER JOIN avion ON vol.na=avion.na
+WHERE
+  avion.nom LIKE 'AIRBUS';
